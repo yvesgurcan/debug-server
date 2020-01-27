f63cb07783d72e993346bc103c1cc5efb8a23139
yvesgurcan <gurcan.yves+github@gmail.com>
2020-01-26 16:43:08 -0800

    ✨ sweeping the dust

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-curl http://localhost:3000/
-curl -X POST http://localhost:3000/
+Template for a Node/Express microservice.
+
+    curl http://localhost:3000/
+    curl -X POST http://localhost:3000/
--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json({ extended: false }));
-app.all('/*', function (req, res, next) {
+app.all('/*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', '*');
     next();
 });
 
 // endpoints
-require('./endpoints/get/root1')(app);
-require('./endpoints/post/root1')(app);
+require('./endpoints/get/root')(app);
+require('./endpoints/post/root')(app);
 
 app.listen(3000, 'localhost', () => console.log(`Debug server listening\n`));
